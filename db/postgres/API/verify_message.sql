3bed17607635ccd5667f78be6af5461503649a9b
Pavel Lebed <pavel.lebed@gs-labs.ru>
2019-04-09 09:35:04 +0300

    #49 Develop game search rocedure - add requests generation

---
+++ db/postgres/API/verify_message.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE PROCEDURE chess.verify_message(
+	p_request_id bigint,
+	p_user_id integer)
+LANGUAGE 'plpgsql'
+
+AS $BODY$
+DECLARE
+BEGIN
+
+  LOCK TABLE ONLY chess.messages;
+  UPDATE chess.messages SET send_time = NULL WHERE request_id = p_request_id AND user_id = p_user_id;
+
+END;
+$BODY$;
