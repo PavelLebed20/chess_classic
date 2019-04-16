2164ddb8ba37f980eea5557d118cd99b67e61375
Pavel-Lebed <chess_boy@mail.ru>
2019-04-16 21:51:21 +0300

    #79 Server message send fix - fix render drop after minimize

--- ChessRender/chess_render.py
+++ ChessRender/chess_render.py
@@ -294,4 +294,6 @@ class Render(ShowBase):
         Render scene function
         :return: NONE.
         """
-        self.taskMgr.step()
+        if self.appRunner is None or self.appRunner.dummy or \
+           (self.appRunner.interactiveConsole and not self.appRunner.initialAppImport):
+            self.taskMgr.run()
