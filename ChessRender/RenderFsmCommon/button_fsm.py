dda685ef93008bed4ab4431c70c5993c6a705e70
Dmitrii Tunikov <dima.tunikov@mail.ru>
2019-04-28 00:12:51 +0300

    101 - fixed images for chess

---
+++ ChessRender/RenderFsmCommon/button_fsm.py
@@ -0,0 +1,13 @@
+
+class ButtonFsm:
+    def __init__(self, title, position, command=None, link_key=None):
+        self.title = title
+        self.command = command
+        self.link_key = link_key
+        self.position = position
+
+    def add_link(self, link_key):
+        self.link_key = link_key
+
+    def add_command(self, command):
+        self.command = command
---
+++ ChessRender/RenderFsmCommon/text_field_fsm.py
@@ -0,0 +1,6 @@
+class TextFieldFsm:
+
+    def __init__(self, title, position, initial_text):
+        self.title = title
+        self.position = position
+        self.initial_text = initial_text
