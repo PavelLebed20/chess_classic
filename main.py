10bdb6d88b064e0f67c7f6a72e0e1e4a3beea661
Lebed_Pavel <pavellebed30@gmail.com>
2019-05-03 19:18:09 +0300

    #120 implement server code authorization - fix mail send

--- main.py
+++ main.py
@@ -8,16 +8,5 @@ import smtplib
 from ChessEngine.chess_engine import Engine
 
 if __name__ == '__main__':
-    try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.ehlo()
-        server.login("chess.classic.official@gmail.com", "ChhessClassicc1488")
-        msg = "Hello"
-        server.sendmail("chess.classic.official@gmail.com", "pavellebed30@gmail.com", msg)
-        server.quit()
-    except:
-        print
-        'Something went wrong...'
-
     engine = Engine()
     engine.render.run()
